14cf256f9e2f808fddab54d46f9f515621caeb9d
daniel.ranghetti <danielranghetti48@gmail.com>
2020-02-27 16:24:57 -0300

    bugflix/Professor

--- src/main/java/com/example/escolahbsis/professor/ProfessorRest.java
+++ src/main/java/com/example/escolahbsis/professor/ProfessorRest.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/professor")
 public class ProfessorRest {
